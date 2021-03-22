75089edb1a30d8dc3c13fd171f4eae07b46f60a8
eismont <eismont.ar@gmail.com>
2021-03-22 04:13:15 +0100

    Added computing median of metrics

--- src/Parsing.py
+++ src/Parsing.py
@@ -3,6 +3,9 @@ from datetime import datetime, timedelta
 import pathlib
 import pandas as pd
 import networkx as nx
+from statistics import median, mean
+from itertools import combinations
+from minepy import MINE
 import warnings
 
 warnings.simplefilter("ignore", UserWarning)
@@ -20,7 +23,7 @@ def loadTraces(pathToTraces):
     traces = {}
     for operation in operations:
         pathToOperation = pathToTraces / operation
-        pathes = list(pathToOperation.glob('*.json'))
+        pathes = sorted(list(pathToOperation.glob('*.json')))
         traces[operation] = {}
         traces[operation]['id'] = list(map(lambda x: x.name[:x.name.find('.json')], pathes))
         traces[operation]['data'] = list(map(lambda x: loadJson(x), pathes))
@@ -84,6 +87,27 @@ def parseTrace(operation, df, graph):
     return G
 
 
+# fix non-endTimestamp problem
+def fixTraces(operations, traces):
+    for operation in operations:
+        for trace in traces[operation]['graph']:
+            spans = trace.nodes(data=True)
+            for span in spans:
+                if span[1]['endTimestamp'] == 'Null':
+                    children = list(nx.descendants(trace, span[0]))
+                    if children == []:
+                        continue
+                    endTimestamp = span[1]['startTimestamp']
+                    for child in children:
+                        time = spans[child]['endTimestamp']
+                        if time != 'Null':
+                            endTimestamp = max(endTimestamp, time)
+                    span[1]['endTimestamp'] = endTimestamp
+                    span[1]['duration'] = span[1]['endTimestamp'] - span[1]['startTimestamp']
+
+    return traces
+
+
 def createWindowing(windowSize, overlapping):
     n_s = int(windowSize * (1 - overlapping))
     windows = []
@@ -101,71 +125,114 @@ def createWindowing(windowSize, overlapping):
     return windows, ds
 
 
-def getLabel5(span):
-    return span['host'] + '_' + span['operation'] + '_' + span['name'] + '_' + span['service'] + '_' + span['project']
+# create label from features
+def combineLabel(features, combination):
+    label = features[0]
+    for i in combination:
+        label = label + '_' + features[i]
+    return label
 
 
-def getLabel2(span):
-    return span['host'] + '_' + span['name']
+def createModes():
+    features_p = ['host_1', 'operation_1', 'name_1', 'service_1', 'project_1']
+    features = ['host_2', 'operation_2', 'name_2', 'service_2', 'project_2']
+    featuresNonCommunication = ['host', 'operation', 'name', 'service', 'project']
+    columns = []
+    columns.append(featuresNonCommunication[0])
+    columns.append(features_p[0] + '->' + features[0])
+    for l in range(1, len(features)):
+        for combination in combinations(list(range(1, len(features))), l):
+            label_r = combineLabel(featuresNonCommunication, list(combination))
+            columns.append(label_r)
+            label_r = combineLabel(features, list(combination))
+            if len(features_p) != 0:
+                label_l = combineLabel(features_p, list(combination))
+                columns.append(label_l + '->' + label_r)
+
+    modes = {}
+    for i in range(len(columns)):
+        k = (i // 2 + 1, i // 2 + 17)[i % 2]
+        modes[k] = {'name': columns[i], 'combinations': []}
+
+    return modes
 
 
-def createColumns(operations, nodeNames, metrics, traces, ds):
-    # aggregation graphs (2 and 5 features) columns
-    combinations = []
+def createColumns(pathToTraces, operations, nodeNames, metrics, traces, modes, ds):
+    def addCombinationToMode(i, label):
+        k = (i // 2 + 1, i // 2 + 17)[i % 2]
+        if label not in modes.get(k).get('combinations'):
+            modes[k]['combinations'].append(label)
+            modes[k]['combinations'].append(label + '__duration')
+
+    def addCombintaionToColumns(label):
+        if label not in list(ds.keys()):
+            ds[label] = 0
+            ds[label + '__duration'] = 0
+
+    # get all possible combinations of two types of aggregation
     for operation in operations:
         for trace in traces[operation]['graph']:
             spans = trace.nodes(data=True)
             for span in spans:
-                if span[1]['isRoot']:
-                    continue
-                span_p = getLabel5(spans[list(trace.predecessors(span[0]))[0]])
-                span_c = getLabel5(span[1])
-                if span_c != span_p:
-                    communication = span_p + '->' + span_c
-                    if communication not in combinations:
-                        combinations.append(communication)
-                        ds[communication] = 0
-                        ds[communication + '__duration'] = 0
+                i = 0
+                features_p = []
+                if not (span[1]['isRoot']):
+                    span_p = spans[list(trace.predecessors(span[0]))[0]]
+                    features_p = [span_p['host'], span_p['operation'], span_p['name'], span_p['service'],
+                                  span_p['project']]
+                features = [span[1]['host'], span[1]['operation'], span[1]['name'], span[1]['service'],
+                            span[1]['project']]
+                addCombintaionToColumns(features[0])
+                addCombinationToMode(i, features[0])
+                i += 1
+                if len(features_p) != 0:
+                    addCombintaionToColumns(features_p[0] + '->' + features[0])
+                    addCombinationToMode(i, features_p[0] + '->' + features[0])
+                i += 1
+                for l in range(1, len(features)):
+                    for combination in combinations(list(range(1, len(features))), l):
+                        label_r = combineLabel(features, list(combination))
+                        addCombintaionToColumns(label_r)
+                        addCombinationToMode(i, label_r)
+                        i += 1
+                        if len(features_p) != 0:
+                            label_l = combineLabel(features_p, list(combination))
+                            addCombintaionToColumns(label_l + '->' + label_r)
+                            addCombinationToMode(i, label_l + '->' + label_r)
+                        i += 1
 
-                span_p = getLabel2(spans[list(trace.predecessors(span[0]))[0]])
-                span_c = getLabel2(span[1])
-                if span_c != span_p:
-                    communication = span_p + '->' + span_c
-                    if communication not in combinations:
-                        combinations.append(communication)
-                        ds[communication] = 0
-                        ds[communication + '__duration'] = 0
+    # save JSON of modes
+    with open(pathToTraces / 'modes.json', 'w') as f:
+        json.dump(modes, f)
 
-    # counting and duration (host_name) columns
-    combinations = []
-    for operation in operations:
-        for trace in traces[operation]['graph']:
-            spans = trace.nodes(data=True)
-            for span in spans:
-                combination = getLabel2(span[1])
-                if combination not in combinations:
-                    combinations.append(combination)
-                    ds[combination] = 0
-                    ds[combination + '__duration'] = 0
-
-    # communications between hosts (for plots)
-    for i in range(len(nodeNames)):
-        for j in range(i + 1, len(nodeNames)):
-            ds[nodeNames[i] + '_' + nodeNames[j] + '__total_number'] = 0
-            ds[nodeNames[i] + '_' + nodeNames[j] + '__total_duration'] = 0
-
-    # total counting and duration per host columns (for plots)
-    for host in nodeNames:
-        ds[host + '__total_number'] = 0
-        ds[host + '__total_duration'] = 0
+    # Metrics columns
+    for metric in metrics:
+        for name in nodeNames:
+            ds[name + '_' + metric] = 0.0
 
     # MI columns
-    for k in range(len(metrics)):
-        for l in range(k, len(metrics)):
+    for p in range(len(metrics)):
+        for l in range(p, len(metrics)):
             for i in range(len(nodeNames)):
-                t = (0, 1)[k == l]
+                t = (0, 1)[p == l]
                 for j in range(i + t, len(nodeNames)):
-                    ds['MI' + '_' + nodeNames[i] + '_' + nodeNames[j] + '_' + metrics[k] + '_' + metrics[l]] = 0.0
+                    ds['MI' + '_' + nodeNames[i] + '_' + metrics[p] + '_' + nodeNames[j] + '_' + metrics[l]] = 0.0
+
+    return ds
+
+
+def computeMedianOfMetric(windowSize, overlapping, nodeNames, metrics, windows, nodes, ds):
+    n_s = int(windowSize * (1 - overlapping))
+    f = 0
+    k = 0
+    while f < len(windows):
+        for metric in metrics:
+            for name in nodeNames:
+                m = median(list(nodes[name]['data'][metric])[k:k + windowSize])
+                # m = mean(list(nodes[name]['data'][metric])[k:k + windowSize])
+                ds.at[f, name + '_' + metric] = m
+        k += n_s
+        f += 1
 
     return ds
 
@@ -182,7 +249,11 @@ def computeMI(windowSize, overlapping, nodeNames, metrics, windows, nodes, ds):
                     for j in range(i + t, len(nodeNames)):
                         mi = mutual_info_score(list(nodes[nodeNames[i]]['data'][metrics[p]])[k:k + windowSize],
                                                list(nodes[nodeNames[j]]['data'][metrics[l]])[k:k + windowSize])
-                        ds.at[f, 'MI' + '_' + nodeNames[i] + '_' + nodeNames[j] + '_' + metrics[p] + '_' + metrics[
+                        # mine = MINE(alpha=0.6, c=15, est="mic_approx")
+                        # mine.compute_score(list(nodes[nodeNames[i]]['data'][metrics[p]])[k:k + windowSize],
+                        #                   list(nodes[nodeNames[j]]['data'][metrics[l]])[k:k + windowSize])
+                        # mi = mine.mic()
+                        ds.at[f, 'MI' + '_' + nodeNames[i] + '_' + metrics[p] + '_' + nodeNames[j] + '_' + metrics[
                             l]] = mi
         k += n_s
         f += 1
@@ -190,107 +261,70 @@ def computeMI(windowSize, overlapping, nodeNames, metrics, windows, nodes, ds):
     return ds
 
 
-def collectData(operations, nodeNames, windows, traces, ds):
+def collectData(operations, windows, traces, ds):
     # find index of window
     def findIndex(time):
         for i in range(len(windows)):
-            if (time <= windows[i][1]) and (time >= windows[i][0]):
+            if windows[i][0] <= time < (windows[i][1] + timedelta(seconds=1)):
                 return i
         return -1
 
-    # get communication label with f function
-    def getCommunication(span, trace, f):
-        if span[1]['isRoot']:
-            return False, ''
-        spans = trace.nodes(data=True)
-        span_p = f(spans[list(trace.predecessors(span[0]))[0]])
-        span_c = f(span[1])
-        if span_c != span_p:
-            communication = span_p + '->' + span_c
-            return True, communication
-        return False, ''
+    def increaseNumberAndDuration(row, column, duration):
+        ds.at[row, column + '__duration'] += duration
+        ds.at[row, column] += 1
 
-    # get communication label between hosts
-    def getCommunicationHosts(span, trace):
-        if span[1]['isRoot']:
-            return False, ''
-        spans = trace.nodes(data=True)
-        span_p = spans[list(trace.predecessors(span[0]))[0]]['host']
-        span_c = span[1]['host']
-        if span_c != span_p:
-            if nodeNames.index(span_p) < nodeNames.index(span_c):
-                communication = span_p + '_' + span_c
-                return True, communication
-            communication = span_c + '_' + span_p
-            return True, communication
-        return False, ''
-
-    def increaseNumberAndDuration(row, column, duration, totalLabel):
-        if totalLabel:
-            ds.at[row, column + '__total_duration'] += duration
-            ds.at[row, column + '__total_number'] += 1
-        else:
-            ds.at[row, column + '__duration'] += duration
-            ds.at[row, column] += 1
-
-    def fillWindow(i_s, i_e, span, column, totalLabel=False):
+    def fillWindow(i_s, i_e, span, column):
         if (i_s == i_e):
-            increaseNumberAndDuration(i_s, column, (span['endTimestamp'] - span['startTimestamp']).microseconds,
-                                      totalLabel)
+            increaseNumberAndDuration(i_s, column,
+                                      (span['endTimestamp'] - span['startTimestamp']) // timedelta(microseconds=1))
         else:
             if (i_e == -1):
-                increaseNumberAndDuration(i_s, column, (windows[i_s][1] - span['startTimestamp']).microseconds,
-                                          totalLabel)
+                increaseNumberAndDuration(i_s, column, (
+                        windows[i_s][1] + timedelta(seconds=1) - span['startTimestamp']) // timedelta(
+                    microseconds=1))
             else:
-                increaseNumberAndDuration(i_s, column, (windows[i_s][1] - span['startTimestamp']).microseconds,
-                                          totalLabel)
-                increaseNumberAndDuration(i_e, column, (windows[i_e][1] - span['endTimestamp']).microseconds,
-                                          totalLabel)
+                increaseNumberAndDuration(i_s, column, (
+                        windows[i_s][1] + timedelta(seconds=1) - span['startTimestamp']) // timedelta(
+                    microseconds=1))
+                increaseNumberAndDuration(i_e, column,
+                                          (span['endTimestamp'] - windows[i_e][0]) // timedelta(microseconds=1))
                 for i in range(1, i_e - i_s):
-                    increaseNumberAndDuration(i_s + i, column,
-                                              (windows[i_s + 1][1] - windows[i_s + 1][0]).microseconds,
-                                              totalLabel)
+                    increaseNumberAndDuration(i_s + i, column, (
+                            windows[i_s + i][1] + timedelta(seconds=1) - windows[i_s + i][0]) // timedelta(
+                        microseconds=1))
 
     for operation in operations:
         for trace in traces[operation]['graph']:
             spans = trace.nodes(data=True)
             for span in spans:
-                ts = span[1]['startTimestamp'].replace(microsecond=0)
-                i_s, i_e = findIndex(ts), -1
+                i_s, i_e = findIndex(span[1]['startTimestamp']), -1
                 if span[1]['endTimestamp'] != 'Null':
-                    te = span[1]['endTimestamp'].replace(microsecond=0)
-                    i_e = findIndex(te)
-
-                # graph with 5 features
-                isCommunication, communication = getCommunication(span, trace, getLabel5)
-                if isCommunication:
-                    fillWindow(i_s, i_e, span[1], communication)
-
-                # graph with 2 features (host_name)
-                isCommunication, communication = getCommunication(span, trace, getLabel2)
-                if isCommunication:
-                    fillWindow(i_s, i_e, span[1], communication)
-
-                # counting and duration per host_name
-                fillWindow(i_s, i_e, span[1], getLabel2(span[1]))
-
-                # communications between hosts
-                isCommunication, communication = getCommunicationHosts(span, trace)
-                if isCommunication:
-                    fillWindow(i_s, i_e, span[1], communication, True)
-
-                # total counting and duration per host
-                fillWindow(i_s, i_e, span[1], span[1]['host'], True)
+                    i_e = findIndex(span[1]['endTimestamp'])
+                features = [span[1]['host'], span[1]['operation'], span[1]['name'], span[1]['service'],
+                            span[1]['project']]
+                fillWindow(i_s, i_e, span[1], features[0])
+                features_p = []
+                if not (span[1]['isRoot']):
+                    span_p = spans[list(trace.predecessors(span[0]))[0]]
+                    features_p = [span_p['host'], span_p['operation'], span_p['name'], span_p['service'],
+                                  span_p['project']]
+                if len(features_p) != 0:
+                    fillWindow(i_s, i_e, span[1], features_p[0] + '->' + features[0])
+                for l in range(1, len(features)):
+                    for combination in combinations(list(range(1, len(features))), l):
+                        label_r = combineLabel(features, list(combination))
+                        fillWindow(i_s, i_e, span[1], label_r)
+                        if len(features_p) != 0:
+                            label_l = combineLabel(features_p, list(combination))
+                            fillWindow(i_s, i_e, span[1], label_l + '->' + label_r)
 
     return ds
 
 
 def saveData(overlapping, pathToTraces, ds):
-    title = 'non'
-    if overlapping != 0:
-        title = str(int(overlapping * 100)) + '%'
+    title = ('non', str(int(overlapping * 100)) + '%')[overlapping != 0]
 
-    ds.to_csv(pathToTraces / ('all_features_from_spans_' + title + '_overlapping.csv'), index=False)
+    ds.to_csv(pathToTraces / ('parsed_traces_with_' + title + '_overlapping.csv'), index=False)
 
 
 def main(windowSize=60, overlapping=0):
@@ -308,10 +342,13 @@ def main(windowSize=60, overlapping=0):
         traces[operation]['graph'] = list(
             map(lambda x: parseTrace(operation, x, nx.DiGraph()), traces[operation]['data']))
 
+    traces = fixTraces(operations, traces)
     windows, ds = createWindowing(windowSize, overlapping)
-    ds = createColumns(operations, nodeNames, metrics, traces, ds)
+    modes = createModes()
+    ds = createColumns(pathToTraces, operations, nodeNames, metrics, traces, modes, ds)
+    ds = computeMedianOfMetric(windowSize, overlapping, nodeNames, metrics, windows, nodes, ds)
     ds = computeMI(windowSize, overlapping, nodeNames, metrics, windows, nodes, ds)
-    ds = collectData(operations, nodeNames, windows, traces, ds)
+    ds = collectData(operations, windows, traces, ds)
 
     saveData(overlapping, pathToTraces, ds)
 
